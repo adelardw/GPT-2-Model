e4ad7fab0f8979a632eab74dd5ff61393786001e
Yaroslav Sergaev <105156233+adelardw@users.noreply.github.com>
2024-09-05 11:26:40 +0300

    Update gpt2.py

--- gpt/gpt2.py
+++ gpt/gpt2.py
@@ -44,31 +44,4 @@ class GPT(nn.Module):
         
         return self.fc(out)
 
-"""vocab_size=13
-max_seq_len=29
-in_size = 256
-out_size = 256
-head_size = 128
-num_heads = 2
-dropout_p = 0.5
-fc_hidden_size = 128
-num_layers = 4
 
-gpt = GPT(vocab_size,
-        max_seq_len,
-        in_size,
-        out_size,
-        head_size,
-        num_heads,
-        dropout_p,
-        fc_hidden_size,
-        num_layers)
-
-
-criterion = nn.CrossEntropyLoss()
-x = torch.randint(3, 13, (64, 17))
-trg = torch.randint(3, 13, (64, 17))
-output = gpt(x).transpose(1,2)
-print(output)
-loss = criterion(output, trg)
-print(loss)"""
